02a1a489d1c449d4eeda068b18c4ecc130453136
Cadel Watson <cadel.watson@agridigital.io>
2019-05-17 11:31:26 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,119 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ApolloProvider, Query } from "react-apollo";
+import ApolloClient, { gql } from "apollo-boost";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GEORA_API_KEY = "o7g##rwefqih^%KKFefifeoiheif#SUPERDUPERSECRETANDSECURElhfushuk%#@$3@!3";
+const GEORA_ACTOR = "ea17e44d-e18a-4d8b-a794-7f169d277b14";
+
+class App extends React.Component {
+  state = {
+    client: new ApolloClient({
+      uri: "http://localhost:3000",
+      headers: { "geora-key": GEORA_API_KEY, "geora-actor": GEORA_ACTOR },
+    }),
+    code: "",
+    coffeeShown: false,
+    dummyState:
+      { "asset": { "version": 56, "location": "Jamie's Farm", "certifiableClaims": [{ "label": "isOrganic", "value": 1, "__typename": "CertifiableClaim" }, { "label": "grade", "value": 5, "__typename": "CertifiableClaim" }, { "label": "price", "value": 100, "__typename": "CertifiableClaim" }, { "label": "variety", "value": 1, "__typename": "CertifiableClaim" }], "auxiliaryClaims": [{ "label": "Farmer name", "value": "Jamie", "__typename": "AuxiliaryClaim" }], "__typename": "Asset" } }
+  }
+
+  render() {
+    return (
+      <ApolloProvider client={this.state.client}>
+
+        {!this.state.coffeeShown ? <div className="App">
+          <header className="App-header">
+            <h1>Painim Kofi</h1>
+
+            <p>Add your coffee code.</p>
+            <input onChange={(e) => this.setState({ code: e.target.value })}></input>
+            <p>{this.state.code}</p>
+            <button onClick={() => this.setState({ coffeeShown: true })}>Find</button>
+
+
+          </header>
+        </div> :
+          <div className="App">
+            <header className="App-header">
+              <button onClick={() => this.setState({ coffeeShown: false })}>Return</button>
+              <h1>Your coffee</h1>
+              <DummyAsset dummyState={this.state.dummyState}></DummyAsset>
+              {/* <Asset version={this.state.code}></Asset> */}
+            </header>
+          </div>}
+
+      </ApolloProvider>
+
+    );
+
+  }
+}
+
+const DummyAsset = ({ dummyState }) => {
+  const data = dummyState;
+
+  return <div>
+    <p>Origin: {data.asset.location}</p>
+    <p>Farmer's name: {getAuxiliaryClaim(data, "Farmer name")}</p>
+    <p>Grade: {getCertifiableClaim(data, "grade")}</p>
+    <p>Variety: {getCertifiableClaim(data, "variety") === 1 ? "Arabica" : "Robusta"}</p>
+  </div>
+}
+
+const Asset = ({ version }) =>
+  <Query query={gql`
+  {
+    asset(version: ${version}) {
+      version
+      location
+
+      certifiableClaims {
+        label
+        value
+      }
+      
+      auxiliaryClaims {
+        label
+        value
+      }
+  
+    }
+  }`}>
+    {({ loading, error, data }) => {
+      if (loading) return <p>Finding beans...</p>;
+      if (error) return <p>Error :(</p>;
+
+      if (data.asset.location === "Unknown") {
+        return <div>
+          <p>Not available</p>
+        </div>
+      } else {
+        return <div>
+          <p>{JSON.stringify(data)}</p>
+          <p>Origin: {data.asset.location}</p>
+          <p>Farmer's name: {getAuxiliaryClaim(data, "Farmer name")}</p>
+          <p>Grade: {getCertifiableClaim(data, "grade")}</p>
+          <p>Variety: {getCertifiableClaim(data, "variety") === 1 ? "Arabica" : "Robusta"}</p>
+        </div>
+      }
+    }}
+
+  </Query>
+
+
+function getCertifiableClaim(data, label) {
+  const claim = data.asset.certifiableClaims.find((claim) => claim.label === label);
+
+  return claim ? claim.value : "Unknown";
+}
+
+
+function getAuxiliaryClaim(data, label) {
+  const claim = data.asset.auxiliaryClaims.find((claim) => claim.label === label);
+
+  return claim ? claim.value : "Unknown";
 }
 
 export default App;
